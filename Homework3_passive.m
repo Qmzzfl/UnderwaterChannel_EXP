5e4c98431aae48a195082f294c7425228a8887d4
Qmzzfl <1024294193@qq.com>
2023-04-07 07:54:58 +0800

    Complete Homework4.m in calculation

--- Homework3_passive.m
+++ Homework3_passive.m
@@ -11,17 +11,17 @@ d_infinity_y = 500;
 d_infinity_z = 100;
 
 % multi-channel simulation setting
-f_end_transfer = 50000;                                     %the end value of f
-df_transfer = 0.01;                                         %delta f
+f_end_transfer = 5000;                                     %the end value of f
+df_transfer = 0.1;                                         %delta f
 f_transfer = 0:df_transfer:f_end_transfer-df_transfer;      %argument f of transfer function
 T_transfer = f_transfer/df_transfer/f_end_transfer;         %argument t of system function(calculate according to DSP theory) 
 global dt;
 dt = 1/f_end_transfer;
 
 %  multi-channel configure
-H_multichannel = 1000;               %deep of shallow sea
-d1_multichannel = 155;               %distance between surface and source
-d2_multichannel = 260;               %distance between surface and hydrophone
+H_multichannel = 100;               %deep of shallow sea
+d1_multichannel = 50;               %distance between surface and source
+d2_multichannel = 50;               %distance between surface and hydrophone
 d_x_multichannel = 500;             %horizontal distance
 zw = 1.5e6;                         %resistance of water
 zb = 3.6e6;                         %resistance of seabed
@@ -43,7 +43,7 @@ LFM = chirp(t_signal,f0_LFM,t_signal_end,f1_LFM);
 LFM_flip = flip(LFM);
 % noise setting
 global SNR;
-SNR = -20;
+SNR = 40;
 
 %% %%%%%%%%%%%%%%%%%% INFINITY SPACE %%%%%%%%%%%%%%%%%%%%
 % time list
@@ -56,7 +56,7 @@ y_rec_CW_infty2 = zeros(1,length(t_observe));
 y_rec_LFM_infty1 = zeros(1,length(t_observe));
 y_rec_LFM_infty2 = zeros(1,length(t_observe));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %%%%%%%%%%%%%%   Tangental     %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   Tangental     %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 d_infty_tang_x = sqrt(d_infinity_y^2+d_infinity_z^2+d_sonar_tang^2);
 %%%%%%%%%%%%%%%%%%%%%%%%%% CW %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -76,7 +76,7 @@ figure(2)
 suptitle("无限大空间中被动声呐水听器切向排布时接收信号与其互相关(LFM脉冲)");
 draw_signal(y_rec_LFM_infty1,y_rec_LFM_infty2,y_LFM_infty_envp,t_observe,t_corr);
 
-%% %%%%%%%%%%%%%%   Normal         %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   Normal         %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 d_infty_norm_x1 = sqrt(d_infinity_y^2+(d_infinity_z-d_sonar_normal)^2);
 d_infty_norm_x2 = sqrt(d_infinity_y^2+(d_infinity_z+d_sonar_normal)^2);
@@ -97,7 +97,7 @@ figure(4)
 suptitle("无限大空间中被动声呐水听器法向排布时接收信号与其互相关(LFM脉冲)");
 draw_signal(y_rec_LFM_infty1,y_rec_LFM_infty2,y_LFM_infty_envp,t_observe,t_corr);
 
-%% %%%%%%%%%%%%%%   radioal         %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   radioal         %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 d_infty_rad_x1 = sqrt(d_infinity_z^2+(d_infinity_y-d_sonar_radial)^2);
 d_infty_rad_x2 = sqrt(d_infinity_z^2+(d_infinity_y+d_sonar_radial)^2);
@@ -123,12 +123,12 @@ draw_signal(y_rec_LFM_infty1,y_rec_LFM_infty2,y_LFM_infty_envp,t_observe,t_corr)
 t_observe_end = 5;
 t_observe = 0:dt:t_observe_end;
 t_ht_end = 5;
-n_ht_end = t_ht_end*5000000/100;
+n_ht_end = t_ht_end*length(T_transfer)*(df_transfer);
 t_conv = 0:dt:t_ht_end+t_signal_end-dt;
 t_corr = -t_conv(end):dt:t_conv(end);
 ref_num = 3;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %%%%%%%%%%%%%%   Tangental     %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   Tangental     %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 d_x_multichannel_tang = sqrt(d_x_multichannel^2+d_sonar_tang^2);
 ht1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
@@ -152,12 +152,12 @@ draw_signal(y_rec_CW_multichannel1,y_rec_CW_multichannel2,y_CW_multichannel_envp
 receive_multichannel(ht1(1:n_ht_end),ht2(1:n_ht_end),LFM);
 % Draw
 figure(8)
-suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(CW脉冲)")
+suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(LFM脉冲)")
 draw_signal(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp,...
             t_conv,t_corr);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %%%%%%%%%%%%%%   Normal     %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   Normal     %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ht1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel-d_sonar_normal,...
                     d_x_multichannel,H_multichannel,c_w,c_b,zw,zb),...
@@ -171,7 +171,7 @@ ht2 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel+d_sonar_n
 receive_multichannel(ht1(1:n_ht_end),ht2(1:n_ht_end),CW); 
 % Draw
 figure(9)
-suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(CW脉冲)")
+suptitle("相干多途信道中被动声呐水听器法向排布时接收信号与其互相关(CW脉冲)")
 draw_signal(y_rec_CW_multichannel1,y_rec_CW_multichannel2,y_CW_multichannel_envp,...
             t_conv,t_corr);
 %%%%%%%%%%%%%%%%%%%%%%%%%%% LFM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -180,12 +180,12 @@ draw_signal(y_rec_CW_multichannel1,y_rec_CW_multichannel2,y_CW_multichannel_envp
 receive_multichannel(ht1(1:n_ht_end),ht2(1:n_ht_end),LFM);
 % Draw
 figure(10)
-suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(CW脉冲)")
+suptitle("相干多途信道中被动声呐水听器法向排布时接收信号与其互相关(LFM脉冲)")
 draw_signal(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp,...
             t_conv,t_corr);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %%%%%%%%%%%%%%   radial         %%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%   radial         %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ht1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
                     d_x_multichannel-d_sonar_radial,H_multichannel,c_w,c_b,zw,zb),...
@@ -199,7 +199,7 @@ ht2 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
 receive_multichannel(ht1(1:n_ht_end),ht2(1:n_ht_end),CW);
 % Draw
 figure(11)
-suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(CW脉冲)")
+suptitle("相干多途信道中被动声呐水听器径向排布时接收信号与其互相关(CW脉冲)")
 draw_signal(y_rec_CW_multichannel1,y_rec_CW_multichannel2,y_CW_multichannel_envp,...
             t_conv,t_corr);
 %%%%%%%%%%%%%%%%%%%%%%%%%%% LFM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -208,12 +208,73 @@ draw_signal(y_rec_CW_multichannel1,y_rec_CW_multichannel2,y_CW_multichannel_envp
 receive_multichannel(ht1(1:n_ht_end),ht2(1:n_ht_end),LFM);
 % Draw
 figure(12)
-suptitle("相干多途信道中被动声呐水听器切向排布时接收信号与其互相关(CW脉冲)")
+suptitle("相干多途信道中被动声呐水听器径向排布时接收信号与其互相关(LFM脉冲)")
 draw_signal(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp,...
             t_conv,t_corr);
 
+%% multi-channel multi exp
+figure(13)
+test_domain = 0:0.1:20;
+j = 1;
+test_domain_len = length(test_domain);
+test_res = zeros(1,test_domain_len);
 
+% radial
+for i = test_domain
+    ht_test1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
+                    d_x_multichannel-i,H_multichannel,c_w,c_b,zw,zb),...
+           'symmetric');
+    ht_test2 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
+                    d_x_multichannel+i,H_multichannel,c_w,c_b,zw,zb),...
+           'symmetric');
+   [y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp] = ...
+        receive_multichannel(ht_test1(1:n_ht_end),ht_test2(1:n_ht_end),LFM);
+%     corr_test = corrcoef(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2);
+%     test_res(j) = abs(corr_test(1,2));
+    test_res(j) = max(y_LFM_multichannel_envp);
+    j = j + 1;
+end
+hold on
+plot(test_domain,test_res);
 
+% normal
+ht_test1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
+                d_x_multichannel,H_multichannel,c_w,c_b,zw,zb),...
+       'symmetric');
+j = 1;
+for i = test_domain
+
+    ht_test2 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel+i,...
+                    d_x_multichannel,H_multichannel,c_w,c_b,zw,zb),...
+           'symmetric');
+   [y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp] = ...
+        receive_multichannel(ht_test1(1:n_ht_end),ht_test2(1:n_ht_end),LFM);
+%     corr_test = corrcoef(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2);
+%     test_res(j) = abs(corr_test(1,2));
+    test_res(j) = max(y_LFM_multichannel_envp);
+    j = j + 1;
+end
+plot(test_domain,test_res);
+
+% tangental
+ht_test1 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
+                d_x_multichannel,H_multichannel,c_w,c_b,zw,zb),...
+       'symmetric');
+j = 1;
+for i = test_domain
+    d_x_multichannel_tang = sqrt(d_x_multichannel^2+i^2);
+    ht_test2 = ifft(Transfer(ref_num,f_transfer,d1_multichannel,d2_multichannel,...
+                    d_x_multichannel_tang,H_multichannel,c_w,c_b,zw,zb),...
+           'symmetric');
+   [y_rec_LFM_multichannel1,y_rec_LFM_multichannel2,y_LFM_multichannel_envp] = ...
+        receive_multichannel(ht_test1(1:n_ht_end),ht_test2(1:n_ht_end),LFM);
+%     corr_test = corrcoef(y_rec_LFM_multichannel1,y_rec_LFM_multichannel2);
+%     test_res(j) = abs(corr_test(1,2));
+    test_res(j) = max(y_LFM_multichannel_envp);
+    j = j + 1;
+end
+plot(test_domain,test_res);
+legend(["径向","法向","切向"])
 
 function [signal1,signal2,signal_xcorr] = receive_infty(empty1,empty2,d1,d2,signal)
     global c_w dt SNR;
--- Homework4.m
+++ Homework4.m
@@ -1,5 +1,62 @@
 clc;clear;close all;
 
-load('E:\ProjectFile\matlab\UnderwaterChannel_EXP\NOF1\mat\NOF1_001.mat')
-pcolor(10*log10(abs(h).^2))
-shading interp
+t_range = linspace(0,32.9,258);
+df_max = 258/32.9/2;
+df_range = linspace(-df_max,df_max,258);
+delay_range = linspace(0,128,2048);
+fai_range = linspace(-8,8,2048);
+load('.\NOF1\mat\NOF1_001.mat');
+
+%% calculate h
+figure(1)
+subplot(231)
+h_abs = mapminmax(abs(h),0,1);
+h_log = 20*log10(h_abs);
+load clown
+image(delay_range,t_range,h_log(1:258,1:4:end),'CDataMapping','scaled');
+colormap(slanCM(153,160))
+caxis([-40 0])
+% colorbar
+
+%% calculate S
+size_h = size(h);
+s = zeros(size(h));
+for i = 1:size_h(2)
+    s(:,i) = abs(fftshift(fft(h(:,i))));
+end
+s_map = s/max(s,[],'all');
+s_log = 20*log10(s_map);
+subplot(232)
+load clown
+image(delay_range,df_range,s_log,'CDataMapping','scaled');
+colormap(slanCM(153,160))
+caxis([-40 0])
+colorbar
+
+%% calculate B
+B = zeros(size_h);
+for i = 1:size_h(1)
+    B(i,:) = fftshift(fft(s(i,:)));
+end
+subplot(233)
+load clown
+image(fai_range,df_range,20*log10(mapminmax(abs(B),0,1)),'CDataMapping','scaled')
+colormap(slanCM(153,160))
+caxis([-40 0])
+colorbar
+
+Rb0_abs = abs(B(:,1025));
+Rb0_map = Rb0_abs/max(Rb0_abs);
+Rb0_log = 20*log10(Rb0_map);
+subplot(223)
+plot(Rb0_log,df_range)
+%% calculate Rh
+p = zeros(1,2048);
+for i = 1:2048
+    p(i) = (sum(s(:,i)))^2;
+end
+subplot(224)
+p_map = p/max(p);
+p_log = 10*log10(p_map);
+plot(p_log);
+
